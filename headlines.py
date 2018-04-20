d19c08397dceab97e2a69a9cb4683fb0b6f6b82c
saurek <s.reklaitis@gmail.com>
2018-04-21 02:32:26 +0300

    adding in Open Weather

--- headlines.py
+++ headlines.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import feedparser
+import json
+import urllib.parse
+import urllib.request
+import urllib3
 
 app = Flask(__name__)
 
@@ -18,7 +22,25 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse((RSS_FEEDS[publication]))
-    return render_template("home.html", articles=feed['entries'])
+    weather = get_weather("London,UK")
+    return render_template("home.html", articles=feed['entries'],
+                           weather=weather)
+
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=47258b3b6592bb72b736e9746e9148d7"
+    query = urllib.parse.quote(query)
+    url = api_url.format(query)
+    data = urllib.request.urlopen(url).read()
+    parsed = json.loads(data.decode('utf-8'))
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":
+                       parsed["weather"][0]["description"],
+                   "temperature": parsed["main"]["temp"],
+                   "city": parsed["name"]
+                   }
+    return weather
 
 
 if __name__ == '__main__':
