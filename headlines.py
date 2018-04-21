b849c18427046d659a5873383aff14d84529c39a
saurek <s.reklaitis@gmail.com>
2018-04-21 04:22:32 +0300

    Updates to weather feed

--- headlines.py
+++ headlines.py
@@ -3,7 +3,6 @@ import feedparser
 import json
 import urllib.parse
 import urllib.request
-import urllib3
 
 app = Flask(__name__)
 
@@ -11,26 +10,44 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640',
-             'wsp': 'http://feeds.washingtonpost.com/rss/politics'}
+             'wsp': 'http://feeds.washingtonpost.com/rss/politics',
+             'tbt': 'http://feeds.feedburner.com/TheBalticTimesNews'}
+
+DEFAULTS = {'publication': 'tbt',
+            'city': 'Vilnius, LT'
+            }
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=47258b3b6592bb72b736e9746e9148d7"
 
 
 @app.route("/")
-def get_news():
-    query = request.args.get("publication")
-    if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
-    else:
-        publication = query.lower()
-    feed = feedparser.parse((RSS_FEEDS[publication]))
-    weather = get_weather("London,UK")
-    return render_template("home.html", articles=feed['entries'],
+def home():
+    # get customized headlines, based on user input or default
+    publiction = request.args.get('publication')
+    if not publiction:
+        publiction = DEFAULTS['publication']
+    articles = get_news(publiction)
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template("home.html", articles=articles,
                            weather=weather)
 
 
+def get_news(query):
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = DEFAULTS['publication']
+    else:
+        publication = query.lower()
+    feed = feedparser.parse((RSS_FEEDS[publication]))
+    return feed['entries']
+
+
 def get_weather(query):
-    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=47258b3b6592bb72b736e9746e9148d7"
     query = urllib.parse.quote(query)
-    url = api_url.format(query)
+    url = WEATHER_URL.format(query)
     data = urllib.request.urlopen(url).read()
     parsed = json.loads(data.decode('utf-8'))
     weather = None
@@ -38,7 +55,8 @@ def get_weather(query):
         weather = {"description":
                        parsed["weather"][0]["description"],
                    "temperature": parsed["main"]["temp"],
-                   "city": parsed["name"]
+                   "city": parsed["name"],
+                   'country': parsed['sys']['country']
                    }
     return weather
 
